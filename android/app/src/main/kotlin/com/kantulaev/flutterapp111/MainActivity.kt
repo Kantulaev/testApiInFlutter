5668afeccd289f4ad82da79fa8337cdafb6a5d88
Kantulaev <49880051+Kantulaev@users.noreply.github.com>
2020-04-08 18:43:13 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/kantulaev/flutterapp111/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kantulaev.flutterapp111
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
